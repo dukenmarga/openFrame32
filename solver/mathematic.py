afd3a07a5ddd23e22d7dfcf6f71ef31fbdebaec7
Duken Marga Turnip <m@duken.info>
2014-01-31 18:54:00 +0700

    Remove class Mathematics, change into collection of function and then add hypotenusa function

--- solver/mathematic.py
+++ solver/mathematic.py
@@ -34,32 +34,41 @@
 
 import math
 
-class Mathematic:
-    def sin(self, x):
-        return math.sin(x*math.pi/180)
-    def cos(self, x):
-        return math.cos(x*math.pi/180)
-    def tan(self, x):
-        return math.tan(x*math.pi/180)
-    def sec(self, x):
-        return 1/(self.sin(x))
-    def cosec(self, x):
-        return 1/(self.cos(x))
-    def cotan(self, x):
-        return 1/(self.tan(x))
-        
-    def S(self, x):
-        return self.sin(x)
-    def C(self, x):
-        return self.cos(x)
-    def T(self, x):
-        return self.tan(x)
-    def SC(self, x):
-        return self.sec(x)
-    def CSC(self, x):
-        return self.cosec(x)
-    def CT(self, x):
-        return self.cotan(x)
-        
-    def p(self, x, y):
-        return math.pow(x, y)
+def sin(x):
+    return math.sin(x*math.pi/180)
+def cos(x):
+    return math.cos(x*math.pi/180)
+def tan(x):
+    return math.tan(x*math.pi/180)
+def sec(x):
+    return 1/(sin(x))
+def cosec(x):
+    return 1/(cos(x))
+def cotan(x):
+    return 1/(tan(x))
+    
+def S(x):
+    return sin(x)
+def C(x):
+    return cos(x)
+def T(x):
+    return tan(x)
+def SC(x):
+    return sec(x)
+def CSC(x):
+    return cosec(x)
+def CT(x):
+    return cotan(x)
+def p(x, y):
+    '''return x^y
+    Example:
+    p(3,2) = 16'''
+    return math.pow(x, y)
+def hypotenuse(a, b):
+    '''return the length of the longest side of
+    right-triangle
+    Example:
+    hypotenuse(3,4) = 5
+    hypotenuse(5,12) = 13
+    '''
+    return math.sqrt(math.pow(a, 2) + math.pow(b, 2))
