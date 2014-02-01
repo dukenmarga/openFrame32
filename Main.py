a2dbf1486d1aeb0a078c6fde342a0df1918e781c
Duken Marga Turnip <m@duken.info>
2014-02-01 23:19:00 +0700

    Add some statements in main program file to model 2d truss

--- Main.py
+++ Main.py
@@ -38,33 +38,46 @@ from model.section import Section
 from model.node import Node
 from model.load import Load
 from solver.truss import Truss
-#import numpy as np
+
+'''
+    Node, element, material, load, and section numbering are using
+    normal increment start from 1 (not Zero), see below.
+    Altough python is using numbering from 0, just
+    let the program do the rest.
+    Main program must be clear and readable and must not confuse people.
+'''
 
 # Define material property.
 material = Material()
-material.addMaterial('concrete', F=30) #material 1
+material.addMaterial('concrete', F=30) #material 1 (not Zero)
 material.addMaterial('steel', F=400, Fu=600) #material 2
 
 # Define section property.
 section = Section()
-section.AddSection(area=100, secondInertia=400, material=1)
-section.AddSection(area=200, secondInertia=500, material=2)
+section.AddSection(area=100, secondInertia=400, material=1) # section 1 using material 1
+section.AddSection(area=200, secondInertia=500, material=2) # section 2 using material 2
 
 # Define node.
 node = Node()
-node.addNode(0,0)
-node.addNode(5,5)
-node.addNode(10,0)
+node.addNode(0,0) # node 1
+node.addNode(5,5) # node 2
+node.addNode(10,0) # node 3
 
 # Define main structure.
 structure = Structure()
-structure.AddElement((1, 2), section=1)
-structure.AddElement((2, 3), section=2)
+structure.AddElement((1, 2), section=1) # node 1 + node 2 using section 1
+structure.AddElement((2, 3), section=2) # node 2 + node 3 using section 2
 
 # Define load.
 load = Load()
-load.addLoad(2, (200, 0))
+load.addLoad(2, (200, 0)) # load 1 at node 2
 
 truss = Truss()
 truss.solve2d(node, material, section, structure, load)
-
+#print "Trigonometri: "
+#print truss.dataTrigonometri
+#print ""
+print "Local Stiffness Matrix:"
+print truss.localStiffnessMatrix
+print "Global Stiffness Matrix"
+print truss.globalStiffnessMatrix
