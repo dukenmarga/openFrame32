7e459a96ec43dcdd3f5c634af63f017ddbcafc0a
Duken Marga Turnip <m@duken.info>
2014-02-04 09:34:16 +0700

    Add documentation class MNode

--- model/node.py
+++ model/node.py
@@ -42,6 +42,23 @@ class Node:
         self.list = np.array([[]])
         pass
     def addNode(self, x, y=0, z=0):
+        '''Add node in coordinate (x, y, z)
+        
+        Parameter
+        ---------
+        x : int
+            Coordinate in x direction
+        y : int, optional
+            Coordinate in y direction (default: 0)
+        z : int, optional
+            Coordinate in z direction (default: 0)
+            
+        Example
+        -------
+        >>> node = Node()
+        >>> node.addNode(12, 3)
+        >>> node.addNode(13, 4, 5)
+        '''
         if self.list.size == 0:
             self.list = np.array([[x, y, z]])
         else:
