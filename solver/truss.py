99f81af4f7c9dd5fe8182587c773bd4cbe1bf9c7
Duken Marga Turnip <dukenmarga@gmail.com>
2014-03-11 22:51:34 +0700

    Tidy up some variables' name

--- solver/truss.py
+++ solver/truss.py
@@ -50,6 +50,22 @@ class Truss():
         pass
     def solve2d(self, model, rec):
         '''Solve Truss 2 dimension'''
+        self.INDEX_AREA = 0
+        self.INDEX_YOUNG_MODULUS = 3
+        self.INDEX_LENGTH = 3
+        self.INDEX_SECTION = 2
+        self.INDEX_MATERIAL = 2
+        self.INDEX_SIN = 0
+        self.INDEX_COS = 1
+        self.INDEX_ANGLE = 2
+        self.INDEX_SPRING_STIFF = 1
+        self.INDEX_NODE_1, self.INDEX_NODE_2 = 0, 1
+        self.INDEX_POSITION_X, self.INDEX_POSITION_Y = 0, 1
+        self.INDEX_NODE = 0
+        self.INDEX_FX = 1
+        self.INDEX_FY = 2
+        self.INDEX_DX = 1
+        self.INDEX_DY = 2
         self.DOF = 2 # degree of freedom each node
         self.totalNode = np.size(model.node.list, 0)
         self.totalMember = np.size(model.structure.list, 0)
@@ -88,36 +104,30 @@ class Truss():
         for element in structure.list:
             if self.totalMember == 0:
                 break
-            # n1 & n2 are number of nodes for each element
-            # ArrayIndexing
-            n1, n2 = element[0]-1, element[1]-1
+            # NODE_1 & NODE_2 are number of nodes for each element
+            NODE_1, NODE_2 = element[self.INDEX_NODE_1]-1, element[self.INDEX_NODE_2]-1
 
-            # a is length of coordinate of node 2 and node 1 in x direction
-            # b is length of coordinate of node 2 and node 1 in y direction
-            # c is real length of node 1 and node 2
-            # iX and iY are index position of coordinates X & Y, ArrayIndexing
-            iX, iY = 0, 1
-            a = node.list[n2][iX] - node.list[n1][iX]
-            b = node.list[n2][iY] - node.list[n1][iY]
-            c = length = maths.hypotenuse(b, a)
+            length_x = node.list[NODE_2][self.INDEX_POSITION_X] - node.list[NODE_1][self.INDEX_POSITION_X]
+            length_y = node.list[NODE_2][self.INDEX_POSITION_Y] - node.list[NODE_1][self.INDEX_POSITION_Y]
+            length_element = maths.hypotenuse(length_y, length_x)
             
             # S=sin C=cos T=tan
-            S = b/c
-            C = a/c
-            T = 0 #a/b
+            S = length_y/length_element
+            C = length_x/length_element
+            T = 0
             
             # Store trigonometri data of each element
             if rec.pre.T.size == 0:
-                rec.pre.T = np.array([[S, C, T, length]])
+                rec.pre.T = np.array([[S, C, T, length_element]])
             else:
-                rec.pre.T = np.append(rec.pre.T, [[S, C, T, length]], axis=0)
+                rec.pre.T = np.append(rec.pre.T, [[S, C, T, length_element]], axis=0)
         
 
         # SPRING
         for spring in restrain.spring:
             # S=sin C=cos T=tan
-            S = maths.sin(spring[2])
-            C = maths.cos(spring[2])
+            S = maths.sin(spring[self.INDEX_ANGLE])
+            C = maths.cos(spring[self.INDEX_ANGLE])
             if C < 1e-10:
                 C = 0
             T = 0
@@ -134,38 +144,32 @@ class Truss():
         Local stiffness is also known as element stiffness
         '''
         i = 0
-        # All index number below are using ArrayIndexing
-        indexArea = 0
-        indexYoungModulus = 2
-        indexLength = 3
-
+        
         # LOCAL STIFFNESS FROM ELEMENT
         structure = model.structure
         section = model.section
         material = model.material
         restrain = model.restrain
         Trig = rec.pre.T
-        
         for element in structure.list:
             if self.totalMember == 0:
                 break
-            #ArrayIndexing
-            numberSection = element[2]-1
             
-            A = section.list[numberSection, indexArea]
-            typeMaterial = section.list[numberSection, indexYoungModulus]-1
-            E = material.list[typeMaterial][3]
-            L = Trig[i][indexLength]
-            B = A * E / L
-
-            S = Trig[i][0]
-            C = Trig[i][1]
+            NUM_SECTION = element[self.INDEX_SECTION]-1
+            A = section.list[NUM_SECTION, self.INDEX_AREA]
+            NUM_MATERIAL = section.list[NUM_SECTION, self.INDEX_MATERIAL]-1
+            E = material.list[NUM_MATERIAL][self.INDEX_YOUNG_MODULUS]
+            L = Trig[i][self.INDEX_LENGTH]
+            K = A * E / L
+            
+            S = Trig[i][self.INDEX_SIN]
+            C = Trig[i][self.INDEX_COS]
             
             matrix = [[C*C,   C*S, -C*C, -C*S],
                       [C*S,   S*S, -C*S, -S*S],
                       [-C*C, -C*S,  C*C,  C*S],
                       [-C*S, -S*S,  C*S,  S*S]]
-            matrix = np.dot(matrix, B)
+            matrix = np.dot(matrix, K)
 
             if rec.pre.localStiffnessMatrix.size == 0:
                 rec.pre.localStiffnessMatrix = np.array([matrix])
@@ -177,10 +181,10 @@ class Truss():
         # LOCAL STIFFNESS FROM SPRING
         i = self.totalMember
         for spring in restrain.spring:
-            k = spring[1]
+            k = spring[self.INDEX_SPRING_STIFF]
 
-            S = Trig[i][0]
-            C = Trig[i][1]
+            S = Trig[i][self.INDEX_SIN]
+            C = Trig[i][self.INDEX_COS]
             
             matrix = [[C*C,   C*S, -C*C, -C*S],
                       [C*S,   S*S, -C*S, -S*S],
@@ -204,15 +208,14 @@ class Truss():
         structure = model.structure
         localStiffnessMatrix = rec.pre.localStiffnessMatrix
         for element in structure.list:
-            # n1 & n2 are number of nodes for each element
-            # ArrayIndexing
-            n1, n2 = element[0], element[1]
+            # NODE_1 & NODE_2 are number of nodes for each element
+            NODE_1, NODE_2 = element[self.INDEX_POSITION_X], element[self.INDEX_POSITION_Y]
             
             #ArrayIndexing
-            a1 = 2*n1-2
-            a2 = 2*n1
-            b1 = 2*n2-2
-            b2 = 2*n2
+            a1 = 2*NODE_1-2
+            a2 = 2*NODE_1
+            b1 = 2*NODE_2-2
+            b2 = 2*NODE_2
             rec.pre.globalStiffnessMatrix[a1:a2, a1:a2] += localStiffnessMatrix[i, 0:2, 0:2]
             rec.pre.globalStiffnessMatrix[a1:a2, b1:b2] += localStiffnessMatrix[i, 0:2, 2:4]
             rec.pre.globalStiffnessMatrix[b1:b2, a1:a2] += localStiffnessMatrix[i, 2:4, 0:2]
@@ -224,9 +227,7 @@ class Truss():
         i = self.totalMember
         restrain = model.restrain
         for spring in restrain.spring:
-            # n is number of spring nodes 
-            # ArrayIndexing
-            n = spring[0]
+            n = spring[self.INDEX_NODE]
             
             #ArrayIndexing
             a1 = 2*n-2
@@ -239,9 +240,9 @@ class Truss():
         rec.pre.loadMatrix = np.array(np.zeros((self.totalDOF,1)))
         load = model.load
         for load in load.list:
-            node = load[0]
-            Fx = load[1]
-            Fy = load[2]
+            node = load[self.INDEX_NODE]
+            Fx = load[self.INDEX_FX]
+            Fy = load[self.INDEX_FY]
             rec.pre.loadMatrix[2*node-2:2*node] += [[Fx], [Fy]]
         pass
     def assembleUnsolvedMatrix(self, model, rec):
@@ -268,10 +269,10 @@ class Truss():
         rec.pre.unsolvedLoadMatrixWithSettlement = rec.pre.unsolvedLoadMatrix
         unsolvedLoadMatrixWithSettlement = rec.pre.unsolvedLoadMatrixWithSettlement
         globalStiffnessMatrix = rec.pre.globalStiffnessMatrix
-        for deformation in restrain.settlement:
-            n = deformation[0]-1
-            dx = deformation[1]
-            dy = deformation[2]
+        for settlement in restrain.settlement:
+            n = settlement[self.INDEX_NODE]-1
+            dx = settlement[self.INDEX_DX]
+            dy = settlement[self.INDEX_DY]
             rec.pre.unsolvedLoadMatrixWithSettlement = \
                 unsolvedLoadMatrixWithSettlement \
                 - globalStiffnessMatrix[n, n] * dx \
@@ -288,18 +289,14 @@ class Truss():
                                                  rec.pre.unsolvedLoadMatrixWithSettlement)
         rec.post.nodeDeformation[rec.pre.unrestrainedNode] = unknownNodeDeformation
         rec.post.nodeDeformation[restrain.list] = 0
-        for deformation in restrain.settlement:
-            n = deformation[0]-1
-            dx = deformation[1]
-            dy = deformation[2]
+        for settlement in restrain.settlement:
+            n = settlement[self.INDEX_NODE]-1
+            dx = settlement[self.INDEX_DX]
+            dy = settlement[self.INDEX_DY]
             rec.post.nodeDeformation[n] =+ dx
             rec.post.nodeDeformation[n+1] += dy
     def solveInternalForceStress(self, model, rec):
         '''Calculate internal force for each node'''
-        # All index number below are using ArrayIndexing
-        indexArea = 0
-        indexYoungModulus = 2
-        indexLength = 3
         i = 0
         structure = model.structure
         section = model.section
@@ -308,32 +305,32 @@ class Truss():
 
         for element in structure.list:
             #NormalIndexing
-            n1, n2 = element[0], element[1]
+            NODE_1, NODE_2 = element[self.INDEX_POSITION_X], element[self.INDEX_POSITION_Y]
             #ArrayIndexing
-            numberSection = element[2]-1
+            NUM_SECTION = element[self.INDEX_SECTION]-1
             
-            A = section.list[numberSection, indexArea]
-            typeMaterial = section.list[numberSection, indexYoungModulus]-1
-            E = material.list[typeMaterial][3]
-            L = Trig[i][indexLength]
-            B = E / L
+            A = section.list[NUM_SECTION, self.INDEX_AREA]
+            NUM_MATERIAL = section.list[NUM_SECTION, self.INDEX_MATERIAL]-1
+            E = material.list[NUM_MATERIAL][self.INDEX_YOUNG_MODULUS]
+            L = Trig[i][self.INDEX_LENGTH]
+            K = E / L
 
-            S = Trig[i][0]
-            C = Trig[i][1]
+            S = Trig[i][self.INDEX_SIN]
+            C = Trig[i][self.INDEX_COS]
                         
             matrix = [[C, S, 0, 0],
                       [0, 0, C, S]]
 
             #Stress
-            s = np.dot(B, np.array([[1,  -1]]))
+            s = np.dot(K, np.array([[1,  -1]]))
             s = np.dot(s, matrix)
-            s = np.dot(s, rec.post.nodeDeformation[np.ix_([2*n1-2,2*n1-1, 2*n2-2,2*n2-1])])
+            s = np.dot(s, rec.post.nodeDeformation[np.ix_([2*NODE_1-2,2*NODE_1-1, 2*NODE_2-2,2*NODE_2-1])])
 
             #Force
-            f = np.dot(B*A, np.array([[1,  -1],
+            f = np.dot(K*A, np.array([[1,  -1],
                                [-1,  1]]))
             f = np.dot(f, matrix)
-            f = np.dot(f, rec.post.nodeDeformation[np.ix_([2*n1-2,2*n1-1, 2*n2-2,2*n2-1])])
+            f = np.dot(f, rec.post.nodeDeformation[np.ix_([2*NODE_1-2,2*NODE_1-1, 2*NODE_2-2,2*NODE_2-1])])
 
             if rec.post.nodeForce.size == 0:
                 rec.post.nodeStress = np.array(s)
